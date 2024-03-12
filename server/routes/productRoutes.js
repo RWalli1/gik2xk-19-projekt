ce58c220bca1c433629bfc8358b7ddcbad244261
J1mb0t <112620895+J1mb0t@users.noreply.github.com>
2024-03-12 14:58:33 +0100

    ADD PUT

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 const db = require("../models");
 const productService = require('../services/productService');
 
+
+// ADD UPUT 
 router.post('/:id/addRating', (req, res) => {
   const rating = req.body;
   const id = req.params.id;
