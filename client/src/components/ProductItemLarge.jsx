972d428233807f9d964c4325140e8526412ac286
J1mb0t <112620895+J1mb0t@users.noreply.github.com>
2024-03-15 15:52:19 +0100

    homepage products

--- client/src/components/ProductItemLarge.jsx
+++ client/src/components/ProductItemLarge.jsx
@@ -1,5 +1,34 @@
-function ProductItemLarge() {
-  return <h3>Product item large</h3>;
+import {
+  Box,
+  Card,
+  CardContent,
+  CardMedia,
+  Paper,
+  Typography,
+} from "@mui/material";
+
+function ProductItemLarge({ product }) {
+  return (
+    <Paper>
+      <Box>
+        <Typography variant="h2">{product.title}</Typography>
+        <Typography>Inlägget publicerades: {"hola"}</Typography>
+      </Box>
+      <Card elevation={0}>
+        <CardMedia
+          component="img"
+          image={product.imageUrl || placeholderImage}
+        />
+        <CardContent>
+          {product.ratings &&
+            product.ratings.map((rating) => (
+              <Tag key={`tag_${rating}`} text={rating} />
+            ))}
+          <Typography variant="body2">{product.body}</Typography>
+        </CardContent>
+      </Card>
+    </Paper>
+  );
 }
 
 export default ProductItemLarge;
--- client/src/components/ProductItemSmall.jsx
+++ client/src/components/ProductItemSmall.jsx
@@ -1,5 +1,55 @@
-function ProductItemSmall() {
-  return <h3>Stuff and stuff</h3>;
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Rating from "@mui/material/Rating";
+import StarIcon from "@mui/icons-material/Star";
+
+//import Rating from " @mui.com/material-ui/react-rating/";
+
+function ProductItemSmall({ product }) {
+  return (
+    <>
+      <Card
+        sx={{
+          minWidth: "30rem",
+          maxWidth: "50rem",
+          minHeight: "5rem",
+        }}
+      >
+        <CardMedia
+          sx={{ height: 300 }}
+          image={product.imageUrl}
+          title={product.title}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {product.title}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {product.description}
+          </Typography>
+          <Typography gutterBottom variant="h5" component="div">
+            {`$${product.price}`}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Add to cart</Button>
+          <Rating
+            name="half-rating-read"
+            defaultValue={2.5}
+            precision={0.5}
+            value={product.averageRating}
+            readOnly
+          />
+          
+        </CardActions>
+      </Card>
+    </>
+  );
 }
 
 export default ProductItemSmall;
